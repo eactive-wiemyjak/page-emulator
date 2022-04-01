127089d55e9b7faf010f16f1a5da317c21a7aa7f
Jakub Lisiecki <jakub@lisiecki.xyz>
2022-04-01 09:49:15 +0200

    done

---
+++ !!!example-url.js
@@ -0,0 +1,5 @@
+/**
+ * Do działania narzędzia należy utworzyć plik url.js który eksportuje string z adresem URL jak na przykładzie:
+ */
+
+module.exports = 'https://example.com/';
--- url.js
+++
@@ -1,9 +0,0 @@
-const urls = [
-    'https://www.imei.info/phonedatabase/phones-vivo/',
-    'https://www.imei.info/phonedatabase/phones-samsung/',
-    'https://www.imei.info/phonedatabase/phones-realme/',
-    'https://www.imei.info/phonedatabase/huawei-y6ii-cam-l21/',
-    'https://www.imei.info/phonedatabase/vivo-1606/'
-];
-
-module.exports = urls[0];
